604dda1f5d6ff018b6f8e4e0f74fa8be1596a347
Salvador Aguinaga <saguinag@nd.edu>
2014-11-04 11:45:47 -0500

    Mac from updates

--- Scripts/crawlUsersTweets.py
+++ Scripts/crawlUsersTweets.py
@@ -32,21 +32,34 @@ for tweet in user_timeline:
 """
     Tweets by SBT followers 
 """
-## Set output file
-outputFile = sbt_fllwrs_tweets
-with open('Data/wsbt_followers.txt') as inputFile:
-    user_timeline = twitter.get_user_timeline(screen_name=scrnName,
-                                              count=2)
-    for tweet in user_timeline:
-        print tweet['text'] + "\n"
-""" 
+def tweets_by(screen_names_file):
+    ## Set output file
+    outputFile = 'tweets_by_followes.txt'
+
+    with open(screen_names_file, 'rb') as inputFile:
+        for scrnName in inputFile:
+            user_timeline = twitter.get_user_timeline(screen_name=scrnName, count=10)
+    #        print json.dumps(user_timeline, indent=2)
+            if user_timeline is not None:
+                for tweet in user_timeline:
+                    print '%s, %s, %s, %s, %s, %s, %s' % (tweet['created_at'],tweet['user']['screen_name'],tweet['user']['id'], tweet['text'],tweet['entities']['hashtags'], tweet['retweet_count'],tweet['favorite_count'])
+            break
+    return
+"""
     Trends near south bend
 """
-closest_trends = twitter.get_closest_trends(lat=41.677101,
-                                            long= -86.269073)
+def trendind_near_south_bend():
+    closest_trends = twitter.get_closest_trends(lat=41.677101,
+                                                long= -86.269073)
 
-trnding_nearby = twitter.get_place_trends(id=closest_trends[0]['woeid'])
-print 'Trends near South Bend as of ',trnding_nearby[0]['created_at']
-for trend  in trnding_nearby[0]['trends']:
-    print json.dumps((trend['name'],trend['url']), indent=2)
+    trnding_nearby = twitter.get_place_trends(id=closest_trends[0]['woeid'])
+    print 'Trends near South Bend as of ',trnding_nearby[0]['created_at']
+    for trend  in trnding_nearby[0]['trends']:
+        print json.dumps((trend['name'],trend['url']), indent=2)
+    return
+################################################################
+##  main
+###############################################################
+if __name__ == "__main__":
+    tweets_by('Data/wsbt_all_followers_screen_names.txt')
 
