127e253ab75263be1a5ba2ce4e63797b926a9eec
Salvador Aguinaga <saguinag@nd.edu>
2014-11-07 07:52:06 -0500

    Updates to the data
    parsed data file from twitter stream and created overview diagram

--- Scripts/wsbTweets.py
+++ Scripts/wsbTweets.py
@@ -9,15 +9,50 @@ with open('Data/tweets.json') as data_file:
         json_object = json.loads(line)
         data.append(json_object)
 
+## parse the twitter stream
+df = pd.DataFrame(columns=['timestamp','name','tweet', 'retweetCnt', 'hastags', 'urlsCnt','mentionsCnt'])
+
+ix = 0
 for jobj in data:
-    #w print json.dumps(jobj)
-#    for user in jobj.iteritems():
-#        print user['screen_name']
-    print '----------------------------'
-    mydata = {  'name': jobj['user']['screen_name'],
-                'text': jobj['text']}
-    print mydata['name'], 'wrote', mydata['text'] # or something
-    break
+    #print '----------------------------'
+        #mydata = {  'name': jobj['user']['screen_name'],
+        #        'text': jobj['text']}
+        #print mydata['name'], 'wrote', mydata['text'] # or something
+        #print json.dumps(jobj,indent=2)
+#    rtstatus = jobj.get('retweeted_status',None)
+#print ix
+    parsedTweet = (jobj.get('created_at',None),
+               jobj.get('user',{}).get('screen_name',None),
+               jobj.get('text',{}),
+               jobj.get('retweeted_status',{}).get('retweet_count',{}),
+               jobj.get('entities',{}).get('hashtags',{}),
+               len(jobj.get('entities',{}).get('urls',{})),
+               len(jobj.get('entities',{}).get('user_mentions',{})))
+    
+#    print '%s, %s, %s, %s, %s, %i, %i' % (jobj.get('created_at',None),
+#                                      jobj.get('user',{}).get('screen_name',None),
+#                                      jobj.get('text',{}),
+#                                      jobj.get('retweeted_status',{}).get('retweet_count',{}),
+#                                          jobj.get('entities',{}).get('hashtags',{}),
+#                                          len(jobj.get('entities',{}).get('urls',{})),
+#                                      len(jobj.get('entities',{}).get('user_mentions',{})))
+    df.loc[ix] = parsedTweet
+    ix +=1
+print df.shape
+
+## time stamp for the ouputfile
+import datetime
+import time
+
+ts = time.time()
+st = datetime.datetime.fromtimestamp(ts).strftime('%d%b%y')
+
+df.to_csv('Data/parsed_wsbt_twitter_stream_%s.txt' % st, sep=',',mode='w',encoding='utf-8',index=False)
+
+##############################################################
+#   Unsed code, but saved in case it's needed
+#
+##############################################################
 #	pprint(dat_json)
 
 #    for line_str in f:
