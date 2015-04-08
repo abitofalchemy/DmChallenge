883da0859024298daecb764c43fa8caf90449be5
abitfoalchemy <saguinag@nd.edu>
2015-04-08 16:58:10 -0400

    GetUsers

--- Scripts/GetUsersTimeline.py
+++ Scripts/GetUsersTimeline.py
@@ -43,38 +43,65 @@ for tweet in user_timeline:
 """
 	Tweets by SBT followers 
 """
+def handle_rate_limiting():
+    app_status = {'remaining':1} # prepopulating this to make the first 'if' check fail
+    while True:
+        wait = 0
+        if app_status['remaining'] &gt; 0:
+            status = twitter.get_application_rate_limit_status(resources = ['statuses', 'application'])
+            app_status = status['resources']['application']['/application/rate_limit_status']
+            home_status = status['resources']['statuses']['/statuses/home_timeline']
+            if home_status['remaining'] == 0:
+                wait = max(home_status['reset'] - time.time(), 0) + 1 # addding 1 second pad
+                time.sleep(wait)
+            else:
+                return
+        else :
+            wait = max(app_status['reset'] - time.time(), 0) + 10
+            time.sleep(wait)
+ 
 def get_timelines(user_ids_filename):
-	## Set output file
-	ts = time.time()
-	st = datetime.datetime.fromtimestamp(ts).strftime('%d%b%y')
+	latest = None   # most recent id scraped
+	try:
+	    last_tweet = tweets.find(limit=1, sort=[('id',-1)])[0] # sort: 1 = ascending, -1 = descending
+	    if last_tweet:
+	        latest = last_tweet['id']
+	except:
+	    print "Error retrieving tweets. Database probably needs to be populated before it can be queried."
+ 
+	no_tweets_sleep = 1
+	while True:
+	    try:
+	        newest = None # this is just a flag to let us know if we should update the "latest" value
+	        params = {'count':200, 'contributor_details':True, 'since_id':latest}
+	        handle_rate_limiting()
+	        home = twitter.get_home_timeline(**params)
+	        if home:
+	            while home:
+	                store_tweets(home)
+	 
+    	            # Only update "latest" if we're inside the first pass through the inner while loop
+    	            if newest is None:
+    	                newest = True
+    	                latest = home[0]['id']
+ 
+    	            params['max_id'] = home[-1]['id'] - 1
+    	            handle_rate_limiting()
+    	            home = twitter.get_home_timeline(**params)
+    	    else:
+    	        time.sleep(60*no_tweets_sleep)
+ 
+    	except TwythonRateLimitError, e:
+    	    reset = int(twitter.get_lastfunction_header('x-rate-limit-reset'))
+    	    wait = max(reset - time.time(), 0) + 10 # addding 10 second pad
+    	    time.sleep(wait)
+    	except Exception, e:
+    	    print e
+    	    print "Non rate-limit exception encountered. Sleeping for 15 min before retrying"
+    	    time.sleep(60*15)
+
 	
-	k = 0
-	tweets_by_users_dict = dict()
-	with open(user_ids_filename, 'rb') as inputFile:
-		for userId in inputFile:
-			userId = userId.rstrip('\r\n')
-			try:
-				#user_timeline = (twitter.get_user_timeline(screen_name='ryanmcgrath', count=2))
-				user_timeline = (twitter.get_user_timeline(user_id=userId, 
-														   count=200, 
-														   include_rts=1) )
-				if (len(user_timeline) > 0):
-					tweets_by_users_dict[userId] = user_timeline 
-			except TwythonAuthError as e:
-				print e
-				break
-				#continue
-		
-			#if k == 3: break
-			#k += 1	
-			# store tweet dump
-	"""
-	print type(user_timeline)			
-	print np.size(user_timeline)
-	pprint.pprint(tweets_by_users_dict.keys()[0])
-	print type(tweets_by_users_dict[tweets_by_users_dict.keys()[0]][0])
-	pprint.pprint(tweets_by_users_dict[tweets_by_users_dict.keys()[0]][0].keys())
-	"""
+	
 	outFile = "Data/tweets_nytimes_followers.dat"
 	titlesMasterDict = shelve.open(outFile, "n")
 	titlesMasterDict.update(masterDict)
